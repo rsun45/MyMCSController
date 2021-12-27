0b44ae73c5dd4fc56ba4962e1615ff78bdf96448
Liber Wang <liberwang@gmail.com>
2021-12-27 11:04:23 -0500

    add website

---
+++ MCSWeb/Actions/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace MCSWeb.Actions {
+    public class Database {
+        public static List<string> getTagContent() {
+            List<string> llistResult = new List<string>();
+            using(SqlConnection conn = new SqlConnection(@"Server=.\SQLExpress;Database=MCS;User Id=mcs;Password=mcs")) {
+                using(SqlCommand comm = conn.CreateCommand()) {
+                    conn.Open();
+                    comm.CommandText = "select tag_cont from dbo.tblTagContent order by id desc";
+                    SqlDataReader dr = comm.ExecuteReader();
+                    while(dr.Read()) {
+                        llistResult.Add(dr[0].ToString());
+                    }
+                }
+            }
+            return llistResult;
+        }
+    }
+}
---
+++ MCSWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using MCSWeb.Actions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCSWeb.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+        public List<string> _listTag {
+            get;
+            set;
+        }
+
+        public IndexModel(ILogger<IndexModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+            this._listTag = Database.getTagContent();
+        }
+    }
+}
