deda358f98f11d3083b13fc0c437ed874669486c
Tony Sun <rsun45@uwo.ca>
2022-03-10 14:28:43 -0500

    create client

--- MCSWebNode/server/index.js
+++ MCSWebNode/server/index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
 
 
 
