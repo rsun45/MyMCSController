a5d6f67aad379d4edaea09965b67560bec119690
Tony Sun <rsun45@uwo.ca>
2022-03-10 12:30:39 -0500

    api test

--- MCSWebNode/server/index.js
+++ MCSWebNode/server/index.js
@@ -9,3 +9,13 @@ const app = express();
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
